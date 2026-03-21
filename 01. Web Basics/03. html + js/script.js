7ec343ba925b4842188285818d2afcb6178a561c
mj <mingjuntan93@gmail.com>
2026-03-22 01:39:04 +0800

    Add mouseenter event listener to button for hover feedback

--- 01. Web Basics/03. html + js/script.js
+++ 01. Web Basics/03. html + js/script.js
@@ -1,6 +1,10 @@
 const button = document.getElementById("myButton");
 const title = document.getElementById("title");
 
+button.addEventListener("mouseenter", function () {
+  button.innerText = "Button hovered!";
+});
+
 button.addEventListener("click", function () {
   title.innerText = "Button clicked!";
 });
